15402de75f5ca4d5cfc5e979246d0aa5ef0fbd15
Mike Thompson <mtthompson11@gmail.com>
2024-04-28 00:04:28 -0500

    start nextjs app, add dependencies

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,9 @@
+"use client";
+// pages/_app.tsx
+import { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ frontend/pages/to-do-list/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Typography } from "@mui/material";
+import dynamic from "next/dynamic";
+
+const ToDoList = () => {
+  return <Typography variant="body1">To Do List</Typography>;
+};
+
+export default ToDoList;
